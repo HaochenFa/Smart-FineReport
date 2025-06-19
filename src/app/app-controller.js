48601d57957bbf51cad970ade87c372e0d468ec3
Haochen Fa <billy.fa@qq.com>
2025-06-19 14:25:16 +0800

    docs: add file-level documentation for app-controller.js
    - Introduced file documentation with descriptions in English and Chinese.
    - Highlighted the core functionality of AI and logic layers using dependency injection.

---
+++ src/app/app-controller.js
@@ -0,0 +1,7 @@
+/**
+ * @file app-controller.js
+ * @author Haochen (Billy) Fa
+ * @description The core of AI & Logic layer, use Dependency Injection technique to listen and control
+ * @description 逻辑和 AI 层的核心，使用「依赖注入」技巧监听和控制组件
+ */
+
